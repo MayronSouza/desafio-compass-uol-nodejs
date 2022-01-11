7b65cd156ae5a12779c610f10abf5680d5b0bd73
MayronSouza <mayron.souza@gmz.solutions>
2022-01-11 17:01:53 -0300

    criando entidade Client

---
+++ src/modules/clients/entities/Clients.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+import { City } from "../../cities/entities/City";
+
+@Entity("clients")
+export class Client {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  full_name: string;
+
+  @Column()
+  gender: string;
+
+  @Column("date")
+  birth_date: Date;
+
+  @Column("integer")
+  age: number;
+
+  @ManyToOne(() => City)
+  @JoinColumn()
+  city: City;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
