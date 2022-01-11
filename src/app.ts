aab05a1c3cf22235a8d0944757223784a4afd1a0
MayronSouza <mayron.souza@gmz.solutions>
2022-01-11 15:18:30 -0300

    Respondendo com um erro genérico, para não quebrar o app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 
 import "./database";
 import "./shared/container";
@@ -10,3 +11,15 @@ export const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error`,
+    });
+  }
+);
