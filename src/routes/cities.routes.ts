2eda5dceb2e4da0d5f4ad7d2bc0db916c7af4fc2
MayronSouza <mayron.souza@gmz.solutions>
2022-01-11 16:40:15 -0300

    refatorando rotas de city

--- src/routes/cities.routes.ts
+++ src/routes/cities.routes.ts
@@ -1,8 +1,14 @@
-import { Router } from "express";
-import { CreateCityController } from "../modules/cities/useCases/CreateCityController";
+// import { Router } from "express";
+// import { CreateCityController } from "../modules/cities/useCases/createCity/CreateCityController";
+// import { FindCityByNameController } from "../modules/cities/useCases/findByName/FindCityByNameController";
+// import { FindCityByStateController } from "../modules/cities/useCases/findByState/FindCityByStateController";
 
-export const cityRouter = Router();
+// export const cityRouter = Router();
 
-const createCityController = new CreateCityController();
+// const createCityController = new CreateCityController();
+// const findCityByNameController = new FindCityByNameController();
+// const findCityByStateController = new FindCityByStateController();
 
-cityRouter.post("/", createCityController.handle);
+// cityRouter.post("/", createCityController.handle);
+// cityRouter.get("/", findCityByNameController.handle);
+// cityRouter.get("/state", findCityByStateController.handle);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,21 @@
+// import { Router } from "express";
+// import { cityRouter } from "./cities.routes";
+
+// export const router = Router();
+
+// router.use("/cities", cityRouter);
+
 import { Router } from "express";
-import { cityRouter } from "./cities.routes";
+import { CreateCityController } from "../modules/cities/useCases/createCity/CreateCityController";
+import { FindCityByNameController } from "../modules/cities/useCases/findByName/FindCityByNameController";
+import { FindCityByStateController } from "../modules/cities/useCases/findByState/FindCityByStateController";
 
 export const router = Router();
 
-router.use("/cities", cityRouter);
+const createCityController = new CreateCityController();
+const findCityByNameController = new FindCityByNameController();
+const findCityByStateController = new FindCityByStateController();
+
+router.post("/cities", createCityController.handle);
+router.get("/cities/names/:name", findCityByNameController.handle);
+router.get("/cities/states/:state", findCityByStateController.handle);
