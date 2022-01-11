74847ae8d1899f1c0262208495359c93ab1b0e92
MayronSouza <mayron.souza@gmz.solutions>
2022-01-11 19:10:19 -0300

    inserindo a rota de criação de client

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,10 @@
-// import { Router } from "express";
-// import { cityRouter } from "./cities.routes";
-
-// export const router = Router();
-
-// router.use("/cities", cityRouter);
-
 import { Router } from "express";
+
 import { CreateCityController } from "../modules/cities/useCases/createCity/CreateCityController";
 import { FindCityByNameController } from "../modules/cities/useCases/findByName/FindCityByNameController";
 import { FindCityByStateController } from "../modules/cities/useCases/findByState/FindCityByStateController";
+
+import { CreateClientController } from "../modules/clients/useCases/createClient/CreateClientController";
 
 export const router = Router();
 
@@ -16,6 +12,10 @@ const createCityController = new CreateCityController();
 const findCityByNameController = new FindCityByNameController();
 const findCityByStateController = new FindCityByStateController();
 
+const createClientController = new CreateClientController();
+
 router.post("/cities", createCityController.handle);
 router.get("/cities/names/:name", findCityByNameController.handle);
 router.get("/cities/states/:state", findCityByStateController.handle);
+
+router.post("/clients", createClientController.handle);
