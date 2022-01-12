8abf037f1bd12251513c33aac13671ea43a28511
MayronSouza <mayron.souza@gmz.solutions>
2022-01-12 19:01:58 -0300

    criando as rotas de listagem, remoção e atualização de cliente

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,9 @@ import { FindCityByStateController } from "../modules/cities/useCases/findByStat
 import { CreateClientController } from "../modules/clients/useCases/createClient/CreateClientController";
 import { FindClientByNameController } from "../modules/clients/useCases/findByName/FindClientByNameController";
 import { FindClientByIdController } from "../modules/clients/useCases/findById/FindClientByIdController";
+import { RemoveClientController } from "../modules/clients/useCases/removeClient/RemoveClientController";
+import { FindClientsCotroller } from "../modules/clients/useCases/find/FindClientsController";
+import { UpdateClientByNameController } from "../modules/clients/useCases/updateNameClient/UpdateClientByNameController";
 
 export const router = Router();
 
@@ -15,13 +18,19 @@ const findCityByNameController = new FindCityByNameController();
 const findCityByStateController = new FindCityByStateController();
 
 const createClientController = new CreateClientController();
+const findClientsController = new FindClientsCotroller();
 const findClientByNameController = new FindClientByNameController();
 const findClientByIdController = new FindClientByIdController();
+const removeClientController = new RemoveClientController();
+const updateClientByNameController = new UpdateClientByNameController();
 
 router.post("/cities", createCityController.handle);
 router.get("/cities/names/:name", findCityByNameController.handle);
 router.get("/cities/states/:state", findCityByStateController.handle);
 
 router.post("/clients", createClientController.handle);
+router.get("/clients", findClientsController.handle);
 router.get("/clients/names/:full_name", findClientByNameController.handle);
 router.get("/clients/:id", findClientByIdController.handle);
+router.delete("/clients/:id", removeClientController.handle);
+router.put("/clients/:id", updateClientByNameController.handle);
